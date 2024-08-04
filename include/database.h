2116b9fa84fe10c5442542a72a7dcd398a7a726b
Ed Nye <edwardjdnye@yahoo.co.uk>
2024-08-04 21:20:22 +0100

    databasefix

--- include/database.h
+++ include/database.h
@@ -5,8 +5,8 @@
 
 //Functions
 int openDatabase(const char *filename, sqlite3 **db);
-int createTable(sqlite3 *db);
-int insertSatelliteData(sqlite3 *db, const char *name, double position[], double velocity[], double acceleration[], double mass, int timestamp);
+int createTable(sqlite3 *db, const char *tableName);
+int insertSatelliteData(sqlite3 *db, const char *tableName, const char *name, double position[], double velocity[], double acceleration[], double mass, int timestamp);
 int closeDatabase(sqlite3 *db);
 
 #endif // DATABASE_H
--- src/database.c
+++ src/database.c
@@ -1,5 +1,6 @@
 #include "/Users/edward/Documents/SatelliteOrbitSimulation/include/database.h"
 #include <stdio.h>
+#include <sqlite3.h>
 
 int openDatabase(const char *filename, sqlite3 **db)  {
     int rc =  sqlite3_open(filename, db);
@@ -10,35 +11,47 @@ int openDatabase(const char *filename, sqlite3 **db)  {
     return 0;
 }
 
-int createTable(sqlite3 *db) {
-    const char *sql = "CREATE TABLE IF NOT EXISTS Satellites ("
-                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                      "name TEXT,"
-                      "timestamp INTEGER,"
-                      "positionX REAL, positionY REAL, positionZ REAL,"
-                      "velocityX REAL, velocityY REAL, velocityZ REAL,"
-                      "accelerationX REAL, accelerationY REAL, AccelerationZ REAL,"
-                      "mass REAL);";
+int createTable(sqlite3 *db, const char *tableName) {
+    char sql[1024];
+    snprintf(sql, sizeof(sql), 
+            "CREATE TABLE IF NOT EXISTS %s ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "name TEXT NOT NULL, "
+            "timestamp INTEGER, "
+            "positionX REAL, "
+            "positionY REAL, "
+            "positionZ REAL, "
+            "velocityX REAL, "
+            "velocityY REAL, "
+            "velocityZ REAL, "
+            "accelerationX REAL, "
+            "accelerationY REAL, "
+            "accelerationZ REAL, "
+            "mass REAL);", tableName);
     
-    char *errMsg = 0;
+    printf("SQL Statement: %s\n", sql);
+    char *errMsg = NULL;
     int rc = sqlite3_exec(db, sql, 0, 0, &errMsg);
+
     if (rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
         return rc;
     }
-    return 0;
+
+    return SQLITE_OK;
 }
 
-int insertSatelliteData(sqlite3 *db, const char *name, double position[], double velocity[], double acceleration[], double mass, int timestamp){
-    const char *sql = "INSERT INTO Satellites (name, timestamp, positionX, positionY, positionZ, velocityX, velocityY, velocityZ, accelerationX, accelerationY, accelerationZ, mass) "
-                      "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+int insertSatelliteData(sqlite3 *db, const char *tableName, const char *name, double position[], double velocity[], double acceleration[], double mass, int timestamp){
+    char sql[512];
+    snprintf(sql, sizeof(sql),
+        "INSERT INTO %s (name, timestamp, positionX, positionY, positionZ, velocityX, velocityY, velocityZ, accelerationX, accelerationY, accelerationZ, mass) "
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", tableName);
     
     sqlite3_stmt *stmt;
-    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Faild to prepare statment: %s\n", sqlite3_errmsg(db));
-        return rc;
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        return 1; // Return error code
     }
 
     sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
@@ -54,14 +67,19 @@ int insertSatelliteData(sqlite3 *db, const char *name, double position[], double
     sqlite3_bind_double(stmt, 11, acceleration[2]);
     sqlite3_bind_double(stmt, 12, mass);
 
-    rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
         fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return 1; // Return error code
     }
+
     sqlite3_finalize(stmt);
-    return rc == SQLITE_DONE ? 0 : rc;
+    return 0; // Success
 }
 
 int closeDatabase(sqlite3 *db) {
-    return sqlite3_close(db);
+    if (db) {
+        sqlite3_close(db);
+    }
+    return 0; // Success
 }
